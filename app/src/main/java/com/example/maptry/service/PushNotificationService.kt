527d4f9a1a98ccf08c8982839f072b1d597679d4
Tommaso Azzalin <azzalintommaso@gmail.com>
2021-10-10 10:41:16 +0200

    Aggiunti log in PushNotificationService

--- app/src/main/java/com/example/maptry/service/PushNotificationService.kt
+++ app/src/main/java/com/example/maptry/service/PushNotificationService.kt
@@ -1,6 +1,7 @@
 package com.example.maptry.service
 
 import android.content.Intent
+import android.util.Log
 import com.example.maptry.domain.Notification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -10,29 +11,38 @@ import kotlinx.coroutines.launch
 import java.lang.Exception
 
 class PushNotificationService: FirebaseMessagingService() {
+    companion object {
+        private const val TAG = "service.PushNotificationService"
+    }
+
     override fun handleIntent(intent: Intent) {
+        Log.v(TAG, "handleIntent")
         super.handleIntent(intent)
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
+        Log.v(TAG, "onMessageReceived")
         super.onMessageReceived(message)
-        println("MESSAGE RECEIVED ")
         println(message.notification?.title)
     }
 
     override fun onDeletedMessages() {
+        Log.v(TAG, "onDeletedMessages")
         super.onDeletedMessages()
     }
 
     override fun onMessageSent(p0: String) {
+        Log.v(TAG, "onMessageSent")
         super.onMessageSent(p0)
     }
 
     override fun onSendError(p0: String, p1: Exception) {
+        Log.v(TAG, "onSendError")
         super.onSendError(p0, p1)
     }
 
     override fun onNewToken(token: String) {
+        Log.v(TAG, "onNewToken")
         super.onNewToken(token)
         CoroutineScope(Dispatchers.IO).launch {
             Notification.addNotificationToken(token)
